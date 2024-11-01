4c14bce606cff1512bdcb23bc8b40e0b3ef51b9a
regis-schuch <43655270+regis-schuch@users.noreply.github.com>
2024-11-01 17:18:36 -0300

    Add files via upload

---
+++ security-multi-compartment-performance/library_a.h
@@ -0,0 +1,8 @@
+//library_a.h
+#ifndef LIBRARY_A_H
+#define LIBRARY_A_H
+
+void parent_process(int pipechan[]);
+
+#endif
+
---
+++ security-multi-compartment-performance/library_b.h
@@ -0,0 +1,8 @@
+//library_b.h
+#ifndef LIBRARY_B_H
+#define LIBRARY_B_H
+
+void rand_str(char *dest, size_t length);
+void child_process(int pipechan[]);
+
+#endif
