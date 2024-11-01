0b0e6239566831a3c942c1dab603d14bf19141b9
regis-schuch <43655270+regis-schuch@users.noreply.github.com>
2024-11-01 17:17:22 -0300

    Delete security-multi-compartment-performance directory

--- security-multi-compartment-performance/callee.h
+++
@@ -1,6 +0,0 @@
-#ifndef CALLEE_H
-#define CALLEE_H
-
-void callee_receive(int read_fd);
-
-#endif
--- security-multi-compartment-performance/trampoline.h
+++
@@ -1,6 +0,0 @@
-#ifndef TRAMPOLINE_H
-#define TRAMPOLINE_H
-
-void trampoline_forward(int read_fd, int write_fd);
-
-#endif
