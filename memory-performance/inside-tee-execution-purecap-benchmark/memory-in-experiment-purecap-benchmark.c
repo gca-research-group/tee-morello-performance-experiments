64010b7e872adb92429ce6f48aa13b676bc9f35a
regis-schuch <43655270+regis-schuch@users.noreply.github.com>
2025-03-06 11:20:34 -0300

    Update memory-in-experiment-purecap-benchmark.c

--- memory-performance/inside-tee-execution-purecap-benchmark/memory-in-experiment-purecap-benchmark.c
+++ memory-performance/inside-tee-execution-purecap-benchmark/memory-in-experiment-purecap-benchmark.c
@@ -1,18 +1,5 @@
 
 /*
- * Programmer   : Regis Rodolfo Schuch
- * Date         : 10 June 2024
- *              : Applied Computing Research Group, Unijui University, Brazil
- *              : regis.schuch@unijui.edu.br
- *              :
- *              : Carlos Molina Jimenez  (carlos.molina@cl.cam.ac.uk)
- * Date         : 12 Aug 2024
- *
- *              : Carlos Molina Jimenez 
- * Date         : 7 Dec 2024
- *              : Carlos Molina Jimenez 
- * Date         : 10 Dec 2024
- *
  * Title        : memory-in-experiment-benchmarkABI-benchmarkABI.c 
  *              :
  * Description  : The memory-out-experiment.c programme carries out memory performance tests, measuring the times required to allocate,
