b7b69f1bda942ae2c5d7be4e4096c145392ce654
regis-schuch <43655270+regis-schuch@users.noreply.github.com>
2025-03-06 11:03:11 -0300

    Delete security-multi-compartment-performance directory

--- security-multi-compartment-performance/library_a.h
+++
@@ -1,8 +0,0 @@
-//library_a.h
-#ifndef LIBRARY_A_H
-#define LIBRARY_A_H
-
-void parent_process(int pipechan[]);
-
-#endif
-
--- security-multi-compartment-performance/library_b.h
+++
@@ -1,8 +0,0 @@
-//library_b.h
-#ifndef LIBRARY_B_H
-#define LIBRARY_B_H
-
-void rand_str(char *dest, size_t length);
-void child_process(int pipechan[]);
-
-#endif
